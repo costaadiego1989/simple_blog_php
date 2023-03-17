501d815526598180f00ba74b4e55c585f8cedd12
Diego <anovaterradecristo@gmail.com>
2023-03-17 15:54:40 -0300

    feat: criado renderizacao da fake api no frontend.

--- blog/index.php
+++ blog/index.php
@@ -1,17 +1,38 @@
 <?php
-    include_once('templates/header.php');
+include_once('templates/header.php');
 ?>
+
 <main>
     <div id="title-container">
         <h1>Blog do Diego Costa</h1>
         <p>O seu blog de programação sobre PHP</p>
     </div>
     <div id="posts-container">
-        <? foreach ($posts as $post): ?>
-            <p><?= $post['title'] ?></p>
-        <? endforeach; ?>
+        <?php foreach ($posts as $post) : ?>
+            <div class="post-box">
+                <a href="<?= $BASE_URL ?>post.php/id=<?= $post['id'] ?>">
+                    <img src="<?= $BASE_URL ?>/img/<?= $post['img'] ?>" alt="<?= $post['title'] ?>">
+                </a>
+                <a href="<?= $BASE_URL ?>post.php/id=<?= $post['id'] ?>">
+                    <h2 class="post-title">
+                        <?= $post['title'] ?>
+                    </h2>
+                </a>
+                <p class="post-description">
+                    <?= $post['description'] ?>
+                </p>
+                <div class="tags-container">
+                    <?php foreach($post['tags'] as $tag): ?>
+                        <a href="<?= $BASE_URL ?>">
+                            <?= $tag ?>
+                        </a>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
 </main>
+
 <?php
-    include_once('templates/footer.php');
+include_once('templates/footer.php');
 ?>
